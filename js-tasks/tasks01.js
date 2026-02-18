f0c1f533539c74287f2a7a833e28384cd124617b
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2026-02-18 17:14:06 +0200

    Add multiple functions for various tasks

--- js-tasks/tasks01.js
+++ js-tasks/tasks01.js
@@ -1,3 +1,34 @@
 // https://www.codewars.com/kata/586c1cf4b98de0399300001d/train/javascript
 
 const combat = (health, damage) => (damage > health ? 0 : health - damage);
+
+
+// Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
+
+// Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+function move (position, roll) {
+  return position + roll * 2;
+}
+
+// Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+function greet (name, owner) {
+  return name === owner ? 'Hello boss' : 'Hello guest';
+}
+
+// Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+// Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+function lovefunc(flower1, flower2){
+  return (flower1 + flower2) % 2 > 0 ? true : false;
+}
