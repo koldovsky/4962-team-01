3016e9b7d93a350a7b6d1bfaf02cbd7154cd269a
Stanislav R. <invicta2008@gmail.com>
2026-02-23 14:50:13 +0000

    js 03 tasks added SR

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -118,3 +118,60 @@ class Dog extends Animal {
     return `Hello ${this.master}`;
   }
 }
+
+
+
+// by Stas R.
+
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+
+function circleCircumference(circle) {
+  const result = 2 * circle.radius * Math.PI;
+  return Number(result.toFixed(6));
+}
+
+//https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+
+function giveMeFive(obj) {
+  let res = [];
+  for (let key in obj) {
+    key.length === 5 ? res.push(key) : null;
+    obj[key].length === 5 ? res.push(obj[key]) : null;
+  }
+  return res;
+}
+
+//https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+class Animal {
+  constructor(name, age, legs, species, status) {
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+}
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "dog", status);
+  }
+  greetMaster() {
+    return `Hello ${this.master}! Woof!`;
+  }
+}
